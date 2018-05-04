084e1cd4af19023900c2565e77805c64d367d183
Egan <1154610080@qq.com>
2018-05-04 21:30:25 +0800

    释放消息资源

---
+++ chapter6/src/main/java/nia/chapter6/DiscardHandler.java
@@ -0,0 +1,19 @@
+package nia.chapter6;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/*
+ * 释放一条入站消息
+ *
+ * @Author Egan
+ * @Date 2018/5/4
+ **/
+public class DiscardHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ReferenceCountUtil.release(msg);
+    }
+}
