c279ed6f547f8c686f61e202ccb4a69eb6a0c145
Egan <1154610080@qq.com>
2018-05-04 21:33:22 +0800

    释放消息资源

--- chapter6/src/main/java/nia/chapter6/DiscardHandler.java
+++ chapter6/src/main/java/nia/chapter6/DiscardHandler.java
@@ -1,5 +1,6 @@
 package nia.chapter6;
 
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.ReferenceCountUtil;
@@ -10,6 +11,7 @@ import io.netty.util.ReferenceCountUtil;
  * @Author Egan
  * @Date 2018/5/4
  **/
+@ChannelHandler.Sharable
 public class DiscardHandler extends ChannelInboundHandlerAdapter {
 
     @Override
